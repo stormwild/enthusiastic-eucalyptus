456b5bd43dad806c15ba400ecfbd34bb1efb55cb
Alexander Torrijos <artorrijos@gmail.com>
2020-03-29 13:32:55 +0800

    Updates

--- ops/after_install.sh
+++ ops/after_install.sh
@@ -7,11 +7,18 @@ npm i
 
 # Not secure, temporary
 # cp /home/ubuntu/.env /home/ubuntu/pcci
-cp /home/ubuntu/.env.development /home/ubuntu/pcci
 # cp /home/ubuntu/.env.staging /home/ubuntu/pcci
 # cp /home/ubuntu/.env.production /home/ubuntu/pcci
 
+FILE=/home/ubuntu/.env.development
+if [ -f "$FILE" ]; then
+  echo "$FILE exists"
+  cp /home/ubuntu/.env.development /home/ubuntu/pcci
+else
+    echo "$FILE does not exist. Please set the .env.development file"
+fi
+
 # Start the app
-# npm run app:build
-# npm run app:start
-# npm run build
+NODE_ENV='test' npm run app:build
+NODE_ENV='development' npm run app:start
+NODE_ENV='development' npm run build
