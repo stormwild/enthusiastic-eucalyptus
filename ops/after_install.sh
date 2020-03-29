0b7db8f778db347900b131bf5cb611339278f88b
Alexander Torrijos <artorrijos@gmail.com>
2020-03-29 13:06:07 +0800

    Updates

--- ops/after_install.sh
+++ ops/after_install.sh
@@ -6,10 +6,10 @@ cd /home/ubuntu/pcci
 npm i
 
 # Not secure, temporary
-cp /home/ubuntu/.env /home/ubuntu/pcci
+# cp /home/ubuntu/.env /home/ubuntu/pcci
 cp /home/ubuntu/.env.development /home/ubuntu/pcci
-cp /home/ubuntu/.env.staging /home/ubuntu/pcci
-cp /home/ubuntu/.env.production /home/ubuntu/pcci
+# cp /home/ubuntu/.env.staging /home/ubuntu/pcci
+# cp /home/ubuntu/.env.production /home/ubuntu/pcci
 
 # Start the app
 # npm run app:build
