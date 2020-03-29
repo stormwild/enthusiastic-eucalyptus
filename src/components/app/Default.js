b126af889d2f4a0242e88b5ce795cbe2c076a905
Alexander Torrijos <artorrijos@gmail.com>
2020-03-29 19:56:43 +0800

    Updated dashboard

--- src/components/app/Default.js
+++ src/components/app/Default.js
@@ -1,12 +1,33 @@
 import React from 'react';
-
-import { Link, safePrefix } from '../../utils';
+import _ from 'lodash';
+import { Link, safePrefix, htmlToReact } from '../../utils';
 
 const Default = props => {
   return (
-    <div>
-      <h1>App Dashboard</h1>
-      <Link to={safePrefix('/app/login')}>Login</Link>
+    <div className="outer">
+      <div className="inner-medium">
+        <article className="post post-full">
+          <header className="post-header">
+            <h1 className="post-title">
+              {_.get(props, 'pageContext.frontmatter.title') || 'Dashboard'}
+            </h1>
+          </header>
+          {_.get(props, 'pageContext.frontmatter.img_path') && (
+            <div className="post-thumbnail">
+              <img
+                src={safePrefix(_.get(props, 'pageContext.frontmatter.img_path'))}
+                alt={_.get(props, 'pageContext.frontmatter.title')}
+              />
+            </div>
+          )}
+          {_.get(props, 'pageContext.frontmatter.subtitle') && (
+            <div className="post-subtitle">
+              {htmlToReact(_.get(props, 'pageContext.frontmatter.subtitle'))}
+            </div>
+          )}
+          <div className="post-content">{htmlToReact(_.get(props, 'pageContext.html'))}</div>
+        </article>
+      </div>
     </div>
   );
 };
